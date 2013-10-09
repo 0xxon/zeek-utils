5d32b0aa7adeb3e670a996b98c414a79d4e08d96
Johanna Amann <johanna@icir.org>
2013-10-09 15:25:54 -0700

    revamp and upgrade psql reader. Now takes database name as argument and can read several logs at once.

--- readToPsql.pl
+++ readToPsql.pl
@@ -8,11 +8,22 @@ use warnings;
 
 use DBI;
 use DBD::Pg qw/:pg_types/;
+use Getopt::Long;
 
-use Carp::Assert;
 use Data::Dumper;
 
-my $connectString = "dbi:Pg:dbname=ssf;port=7779";
+my $dbname;
+my $port;
+
+GetOptions(
+	"db=s" => \$dbname,
+	"port=i" => \$port,
+);
+
+die("Please specify db name with --db=[name]") unless defined($dbname);
+
+my $connectString = "dbi:Pg:dbname=$dbname";
+$connectString .= ";port=$port" if defined($port);
 
 my $dbh = DBI->connect($connectString, "", "", {
 	RaiseError => 1,
@@ -36,143 +47,150 @@ my %typemap = (
 	func => "text",
 );
 
-say Dumper(\@ARGV);
+for my $file ( @ARGV ) {
+	say "Reading $file";
+	croak("File $file does not exist") unless ( -f $file );
+	next if ( -s $file == 0 );
 
-# black magic from the internet... make all .gz and .bz2 arguments go through gzcat.
-s{ 
-    ^            # make sure to get whole filename
-    ( 
-      [^'] +     # at least one non-quote
-      \.         # extension dot
-      (?:        # now either suffix
-	  gz
-	| Z 
-       )
-    )
-    \z           # through the end
-}{zcat '$1' |}xs for @ARGV;
+	# black magic from the internet... make all .gz and .bz2 arguments go through gzcat.
+	$file =~ s{ 
+	    ^            # make sure to get whole filename
+	    ( 
+	      [^'] +     # at least one non-quote
+	      \.         # extension dot
+	      (?:        # now either suffix
+		  gz
+		| Z 
+	       )
+	    )
+	    \z           # through the end
+	}{zcat '$1' |}xs;
 
-s{ 
-    ^            # make sure to get whole filename
-    ( 
-      [^'] +     # at least one non-quote
-      \.         # extension dot
-      (?:        # now either suffix
-	  bz2 
-       )
-    )
-    \z           # through the end
-}{bzcat '$1' |}xs for @ARGV;
+	$file =~ s{ 
+	    ^            # make sure to get whole filename
+	    ( 
+	      [^'] +     # at least one non-quote
+	      \.         # extension dot
+	      (?:        # now either suffix
+		  bz2 
+	       )
+	    )
+	    \z           # through the end
+	}{bzcat '$1' |}xs;
 
+	open(my $fh, $file);
 
-# -------------------------------- Step 1 - parse header lines
+	# -------------------------------- Step 1 - parse header lines
 
-for ( 1..4 ) {
-	# throw away unneeded header lines
-	<>;
-}
-
-my $path = <>;
-chomp($path);
-assert($path =~ s/#path\s//);
-
-my $fields_string = <>;
-$fields_string = <> if ($fields_string =~ m/^#open\s.*/);
-chomp($fields_string);
-assert($fields_string =~ s/#fields\s//);
-
-my @fields = split(/\s/, $fields_string);
-
-my $types_string = <>;
-chomp($types_string);
-assert($types_string =~ s/#types\s//);
-
-my @types = split(/\s/, $types_string);
-
-my %f;
-@f{@fields} = @types;
-
-
-
-# -------------------------------- Step 2 - create table
-
-my $create = "CREATE TABLE IF NOT EXISTS $path (\n";
-$create .= "id SERIAL UNIQUE NOT NULL PRIMARY KEY";
-
-for my $field ( @fields ) {
-	my $type = $f{$field};
-
-	$field =~ tr/./_/; # manipulation is SAVED TO ARRAY! which we want.
-
-	$create .= ",\n";
-	$create .= "$field ";
-
-	if ( $type =~ s#^(table|vector)\[(.*)\]#$2# ) {
-		assert(defined($typemap{$type}));
-		$create .= $typemap{$type}."[]";
-	} else {
-		assert(defined($typemap{$type}));
-		$create .= $typemap{$type};
+	for ( 1..4 ) {
+		# throw away unneeded header lines
+		<$fh>;
 	}
-}
-$create .= "\n);";
 
-#say $create;
+	my $path = <$fh>;
+	chomp($path);
+	croak("Problem parsing header - #path") unless($path =~ s/#path\s//);
 
-$dbh->do($create);
+	my $fields_string = <$fh>;
+	$fields_string = <$fh> if ($fields_string =~ m/^#open\s.*/);
+	chomp($fields_string);
+	croak("Problem parsing header - #fields") unless ($fields_string =~ s/#fields\s//);
 
-my $neednum = scalar @fields;
+	my @fields = split(/\s/, $fields_string);
+
+	my $types_string = <$fh>;
+	chomp($types_string);
+	croak("Problem parsing header - #types") unless ($types_string =~ s/#types\s//);
+
+	my @types = split(/\s/, $types_string);
+
+	my %f;
+	@f{@fields} = @types;
 
 
-# -------------------------------- Step 2 - build copy statement 
+	# -------------------------------- Step 2 - create table
 
-my $insert = "copy $path (".join(',', @fields).") FROM STDIN;";
-$dbh->do($insert);
+	my $create = "CREATE TABLE IF NOT EXISTS $path (\n";
+	$create .= "id SERIAL UNIQUE NOT NULL PRIMARY KEY";
 
-while ( my $line = <> ) {
-	chomp($line);
-	next if ( $line =~ m/^#close\s.*/ );
-	next if ( $line =~ m/^#.*\s.*/ );
+	for my $field ( @fields ) {
+		my $type = $f{$field};
 
-	my @values = split('\t', $line);
-	if ( scalar @values != $neednum ) {
-		say "Column with wrong number of entries";
-		say $line;
-		last;
-	}
-	#say Dumper(\@values);
-	my $str;
+		$field =~ tr/./_/; # manipulation is SAVED TO ARRAY! which we want.
+		$field = 'from_addr' if $field eq 'from';
+		$field = 'to_addr' if $field eq 'to';
+		
 
-	my $pos=0;
-	my @out;
-       	for my $val (@values) {
-		$val = $dbh->quote($val);
-		$val = substr($val, 1, -1);
-		if ( $val eq "-" ) { 
-			push(@out, '\N'); 
-		} elsif ( $val eq "(empty)" ) {
-			push(@out, '{}');
+		$create .= ",\n";
+		$create .= "$field ";
+
+		if ( $type =~ s#^(table|vector)\[(.*)\]#$2# ) {
+			carp("internal error") unless(defined($typemap{$type}));
+			$create .= $typemap{$type}."[]";
 		} else {
-			if ( $types[$pos] =~ m#\[# ) {
-				$val =~ s/"/\\\\\"/g;
-				#die ($val) if $val =~ m#"#;
-				my @parts = split(',', $val);
-				my @parts2 = map { '{"'.$_.'"}' } @parts;
-				push(@out, '{'.join(',', @parts2).'}');
-			} else {
-				push(@out, $val);
-			}
+			carp("internal error") unless(defined($typemap{$type}));
+			$create .= $typemap{$type};
+		}
+	}
+	$create .= "\n);";
 
-		} 
-		$pos++;
+	#say $create;
+
+	$dbh->do($create);
+
+	my $neednum = scalar @fields;
+
+
+	# -------------------------------- Step 2 - build copy statement 
+
+	my $insert = "copy $path (".join(',', @fields).") FROM STDIN;";
+	$dbh->do($insert);
+
+	while ( my $line = <$fh> ) {
+		chomp($line);
+		next if ( $line =~ m/^#close\s.*/ );
+		next if ( $line =~ m/^#.*\s.*/ );
+
+		my @values = split('\t', $line);
+		if ( scalar @values != $neednum ) {
+			say "Column with wrong number of entries";
+			say $line;
+			last;
+		}
+		#say Dumper(\@values);
+		my $str;
+
+		my $pos=0;
+		my @out;
+		for my $val (@values) {
+			$val = $dbh->quote($val);
+			$val = substr($val, 1, -1);
+			if ( $val eq "-" ) { 
+				push(@out, '\N'); 
+			} elsif ( $val eq "(empty)" ) {
+				push(@out, '{}');
+			} else {
+				if ( $types[$pos] =~ m#\[# ) {
+					$val =~ s/"/\\\\\"/g;
+					#die ($val) if $val =~ m#"#;
+					my @parts = split(',', $val);
+					my @parts2 = map { '{"'.$_.'"}' } @parts;
+					push(@out, '{'.join(',', @parts2).'}');
+				} else {
+					push(@out, $val);
+				}
+
+			} 
+			$pos++;
+		}
+
+		#say Dumper(\@out);
+
+		$dbh->pg_putcopydata(join("\t", @out)."\n");
 	}
 
-	#say Dumper(\@out);
+	close($fh);
 
-	$dbh->pg_putcopydata(join("\t", @out)."\n");
-
+	$dbh->pg_putcopyend();
 
 }
-
-$dbh->pg_putcopyend();
-say "Done";
