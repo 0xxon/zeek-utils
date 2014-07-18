34d70d3b7fbe34aec9b62a0fe660cfcdc1ecaa8b
Johanna Amann <johanna@icir.org>
2014-07-18 12:22:10 -0700

    add short script that can convert the abuse.ch ssl blacklist to bro log format

---
+++ convert-blacklist.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/env perl
+# Convert abuse.ch SSL Fingerprint Blacklist in CVS format to format ingestible by
+# the Bro intelligence framework.
+#
+# Get the blacklist at https://sslbl.abuse.ch/blacklist/, get bro at https://bro.org
+#
+# Usage:
+# ./convert-blacklist.pl infile > outfile
+
+use 5.12.0;
+use strict;
+#use warnings; no warnings - they trigger incorrectly here
+
+use Carp;
+
+say join("\t", qw/#fields indicator indicator_type meta.source meta.desc meta.url/);
+while ( my $line = <> ) {
+  chomp($line);
+  next if ( $line =~ m@^#@ );
+  my ( $ts, $hash, $reason) = split(/,/, $line);
+  croak("Invalid line: $line") unless ( defined($ts) && defined($hash) && defined($reason) );
+  say join("\t", ($hash, "Intel::FILE_HASH", "abuse.ch SSLBL", $reason, "https://sslbl.abuse.ch/blacklist/"));
+}
